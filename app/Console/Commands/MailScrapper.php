f95c70f50d16ee81956517eacad0a08148842897
Verbovskiy Alexandr <cofeek5@gmail.com>
2023-05-30 11:36:31 +0300

    some fixes

--- app/Console/Commands/MailScrapper.php
+++ app/Console/Commands/MailScrapper.php
@@ -7,24 +7,28 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\DomCrawler\Crawler;
 
-class MailScrapper extends Command {
+class MailScrapper extends Command
+{
 
     protected $signature = 'command:mail';
 
-    public function handle(TrelloController $controller){
-        $scriptUrl = env("GOOGLE_SCRIPT_LINK")??null;
-        if(!$scriptUrl) {
+    public function handle(TrelloController $controller)
+    {
+        $scriptUrl = env("GOOGLE_SCRIPT_LINK") ?? null;
+        if (!$scriptUrl) {
             var_dump("hasn't");
             return;
         }
+
+        var_dump($scriptUrl);
 
         $sender_filter = "<noreply@e.fiverr.com>";
         $subject_filter = "You've received messages from";
 
         $data = array(
-            "senderFilter"=>$sender_filter,
-            "subjectFilter"=>$subject_filter,
-            "timeFilter"=>60000//60000 - це одна хвилина
+            "subjectFilter" => $subject_filter,
+            "senderFilter" => $sender_filter,
+            "timeFilter" => 60000 * 12 * 60//60000 - це одна хвилина
         );
 
         $ch = curl_init($scriptUrl);
@@ -33,36 +37,74 @@ class MailScrapper extends Command {
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         $result = curl_exec($ch);
         $result = json_decode($result, true);
-
-        //<a href='read.php?id={$inbox['id']}'>
-
-        if(!$result) return;
+        if (!$result) return;
 
         $objects_to_send = [];
-        foreach ($result as $inbox) {
-            //Storage::put($inbox['subject'], $inbox['body']);
+
+        //var_dump($result[0]);
+
+        foreach ($result as $key => $inbox) {
+            if (!$inbox['body']) continue;
+            //Storage::put($inbox['id'] . "(" . $inbox['subject'] . ").html", $inbox['body']);
+
             $crawler = new Crawler($inbox['body']);
+            $email_template = null;
+
+            $content = $crawler->filter(".responsive-table");
+            if ($content->count() < 1) {
+                $content = $crawler->filter(".content-section>table>tbody>tr>td>table>tbody>tr");
+                if ($content->count() < 1) {
+                    Storage::put("_ERROR_" . $inbox['id'] . "(" . $inbox['subject'] . ")" . ".html", $inbox['body']);
+                    continue;
+                }
+                $email_template = 2;
+
+                $filteredArray = [];
+                $content->each(function ($node) use (&$filteredArray) {
+                    $filteredArray[] = $node;
+                });
+
+                $message = array_slice($filteredArray, -3, 1)[0];
+                $message = $message->text();
+            } else {
+                $email_template = 1;
+                $content = $content->filter(".content table");
+                if ($content->count() < 1) {
+                    Storage::put("_ERROR_" . $inbox['id'] . "(" . $inbox['subject'] . ")" . ".html", $inbox['body']);
+                    continue;
+                }
+                $message = $content->text();
+            }
+
+            if (is_null($email_template)) {
+                Storage::put("_ERROR_" . $inbox['id'] . "(" . $inbox['subject'] . ")" . ".html", $inbox['body']);
+                continue;
+            }
+
             $type = "lead";
+            if ($email_template == 1) {
+                $link = $crawler->filter("a[href*='www.fiverr.com/'][href*='linker'][href*='email_name=consolidated_messages']");
+            } else if ($email_template == 2) {
+                $link = $crawler->filter("a[href*='www.fiverr.com/inbox']");
+            }
 
-            $content = $crawler->filter(".content .content");
-            if ($content->count() < 1) continue;
-            $message = $content->text();
-
-            $link = $crawler->filter("a[name='CTA']");
-            if ($link->count() < 1) continue;
+            if (count($link) < 1) {
+                Storage::put("_ERROR_" . $inbox['id'] . "(" . $inbox['subject'] . ")" . ".html", $inbox['body']);
+                continue;
+            }
 
             $link = $link->attr("href");
-            if(str_contains($link, "order_id")) $type = "order";
+            if (str_contains($link, "order_id")) $type = "order";
 
             $name = explode($subject_filter, $inbox['subject']);
-            if(count($name)<2) continue;
+            if (count($name) < 2) continue;
 
             $user_name = trim($name[1]);
-            $objects_to_send[] = ["client"=>$user_name, "message"=>$message, "order_link" => $link, "type"=>$type];
+            $objects_to_send[] = ["client" => $user_name, "message" => $message,
+                "order_link" => $link, "type" => $type, "time" => $inbox["time"]];
         }
-        var_dump($objects_to_send);
 
-        if(count($objects_to_send)<1) return;
-       return $controller->checkMessage($objects_to_send);
+        if (count($objects_to_send) < 1) return;
+        return $controller->checkMessage($objects_to_send);
     }
 }
